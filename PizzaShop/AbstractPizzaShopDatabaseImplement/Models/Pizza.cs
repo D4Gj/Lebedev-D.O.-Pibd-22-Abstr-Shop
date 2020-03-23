0f64b27d9b9a58d06237671b3cd550fbf1579f87
D4Gj <inzadimonax@gmail.com>
2020-03-23 12:49:09 +0400

    started lab 3

--- PizzaShop/AbstractPizzaShopDatabaseImplement/Models/Pizza.cs
+++ PizzaShop/AbstractPizzaShopDatabaseImplement/Models/Pizza.cs
@@ -15,9 +15,7 @@ namespace PizzaShopDatabaseImplement.Models
 
         [Required]
         public decimal Price { get; set; }
-        [ForeignKey("PizzaId")]
         public virtual List<PizzaIngridient> PizzaIngridients { get; set; }
-        [ForeignKey("OrderId")]
         public virtual List<Order> Orders { get; set; }
     }
 }
