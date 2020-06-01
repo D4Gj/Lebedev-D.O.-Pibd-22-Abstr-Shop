12960b606370b8e5bf5c436009117a17727defea
D4Gj <inzadimonax@gmail.com>
2020-06-02 01:43:51 +0400

    created lab8

--- PizzaShop/PizzaShopBusinessLogic/ViewModels/MessageInfoViewModel.cs
+++ PizzaShop/PizzaShopBusinessLogic/ViewModels/MessageInfoViewModel.cs
@@ -3,29 +3,38 @@ using System.Collections.Generic;
 using System.Text;
 using System.Runtime.Serialization;
 using System.ComponentModel;
+using PizzaShopBusinessLogic.Attributes;
 
 namespace PizzaShopBusinessLogic.ViewModels
 {
     [DataContract]
-    public class MessageInfoViewModel
+    public class MessageInfoViewModel : BaseViewModel
     {
         [DataMember]
         public string MessageId { get; set; }
 
-        [DisplayName("Отправитель")]
+        [Column(title: "Отправитель", width: 150)]
         [DataMember]
         public string SenderName { get; set; }
 
-        [DisplayName("Дата письма")]
+        [Column(title: "Дата письма", width: 100)]
         [DataMember]
         public DateTime DateDelivery { get; set; }
 
-        [DisplayName("Заголовок")]
+        [Column(title: "Заголовок", width: 150)]
         [DataMember]
         public string Subject { get; set; }
 
-        [DisplayName("Текст")]
+        [Column(title: "Текст", gridViewAutoSize: GridViewAutoSize.Fill)]
         [DataMember]
         public string Body { get; set; }
+        public override List<string> Properties() => new List<string>
+        {
+            "MessageId",
+            "SenderName",
+            "DateDelivery",
+            "Subject",
+            "Body"
+        };
     }
 }
