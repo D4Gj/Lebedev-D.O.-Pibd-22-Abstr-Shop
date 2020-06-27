f9d7f78f78f0c55fec03049e3d327268f3c3dc94
D4Gj <inzadimonax@gmail.com>
2020-06-27 11:23:37 +0400

    some

--- PizzaShop/AbstractPizzaShopDatabaseImplement/Migrations/20200627071439_Storages.Designer.cs
+++ PizzaShop/AbstractPizzaShopDatabaseImplement/Migrations/20200627071439_Storages.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PizzaShopDatabaseImplement;
 
 namespace PizzaShopDatabaseImplement.Migrations
 {
     [DbContext(typeof(PizzaShopDatabase))]
-    partial class PizzaShopDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20200627071439_Storages")]
+    partial class Storages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
